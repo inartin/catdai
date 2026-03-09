02bb81b387ba53a6815ee6050086dafb640a0762
Artin <17723007+inartin@users.noreply.github.com>
2026-03-09 17:35:47 +0200

    pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "catdai",
-      script: "node_modules/.bin/next",
+      script: "node_modules/next/dist/bin/next",
       args: "start --port 3000",
       instances: 1,
       autorestart: true,
